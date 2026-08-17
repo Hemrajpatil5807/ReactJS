215bd4e589aebc6db0d1aee1c78a4a7c43c71273
Hemraj Patil <hemrajpatil5807@gmail.com>
2026-08-17 22:54:07 +0530

    HTML and JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// creating HTML element using JS
+var h1 = document.createElement('h2');
+// adding text to the element we can also use textContent to add text to the element
+h1.innerHTML = "Hello From JS";
+// appending the element to the body of the document
+document.body.appendChild(h1);
