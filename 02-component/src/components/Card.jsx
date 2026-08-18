8b5c54cd305443dbdb0ccb1dac462eb78c3e1c02
Hemraj Patil <hemrajpatil5807@gmail.com>
2026-08-18 23:38:59 +0530

    Learn Component

---
+++ 02-component/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <img src="https://images.unsplash.com/photo-1786884263909-c355a44d4bce?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile" />
+       <h1>Hemraj Patil</h1>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, soluta?</p>
+       <button>View Profile</button>
+    </div>
+  )
+}
+
+export default Card
